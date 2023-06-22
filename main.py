7cbee0345d07ca6c5623d78d11661687605424b9
RomarioM88 <127489623+RomarioM88@users.noreply.github.com>
2023-06-22 19:48:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,24 @@ class Avto:
         return f" Марка авто: '{self.model}'. Мощность движка: {self.value} л.с."
     def __repr__(self):
         return f"{self.__class__.__name__}(model={self.model!r}).(value={self.value!r})."
-    def engine(self, value: int):
-        self.value = value
+    def engine(self):
+        print("Двигатель автомобиля")
 
 class Car(Avto):
     """ Дочерний класс car наследуемый от avto. """
-    def __init__(self, model: str, color: str):
-        super().__init__(model, value=64) # Поменялся объем двигателя на 1.3 л.
+    def __init__(self, color: str, model,  value: int):
+        super().__init__(model, value) # Поменялся объем двигателя на 1.3 л.
         self.color = color
     def __str__(self):
         return f"Марка авто:'{self.model}'. Цвет : '{self.color}'. Мощность движка: {self.value} л.с."
     def __repr__(self):
         return f"{self.__class__.__name__}(model={self.model!r}).(color={self.color!r}). (value={self.value!r})"
-
+    def engine(self):
+        print("Двигатель легкого автомобиля")
 if __name__ == "__main__":
     a = Avto("Жигули", 75)
     print(a)
-    acar = Car("Запорожец", "Синяя")
+    print(a.engine())
+    acar = Car("Запорожец", "Синий", 65)
     print(acar)
+    print(acar.engine())
